49a8d7d59a9dfb23af1c88cc57fbf52f48fe5f61
StackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2021-10-11 04:45:34 +0200

    See desc for more.
    - Applied theme-handler changes from Sim2Wiki.
    - Sim2Editor -> sim2editor.
    - Get rid of unused things.

--- assets/js/theme-handler.js
+++ assets/js/theme-handler.js
@@ -1,37 +1,19 @@
-/*
-	This implementation is based of Pk11's theme.js, however reworked in my style. ;P
-
-	https://github.com/Epicpkmn11/pk11.us/blob/master/assets/js/theme.js
-
-	Credits goes to Pk11 for it!
-*/
+---
+---
 
 /* Loads a theme. */
 function LoadTheme() {
-	const CurTheme = document.getElementById("themeCSS");
-
-	if (!localStorage.theme || localStorage.theme == "default") {
-		if (CurTheme) CurTheme.remove();
-
-	} else {
-		if (CurTheme) CurTheme.href = "/assets/css/" + localStorage.theme + ".css";
-		else {
-			const Link = document.createElement("link");
-			Link.rel = "stylesheet";
-			Link.href = "/assets/css/" + localStorage.theme + ".css";
-			Link.id = "themeCSS";
-
-			document.head.appendChild(Link);
-		}
-	}
+    if (!localStorage.theme || localStorage.theme == "default") localStorage.theme = "green"; // Set to default theme - green.
+    document.getElementById("themeCSS").href = "/assets/css/" + localStorage.theme + ".css";
 };
 
 /* Sets a theme. */
 function SetTheme(Theme) {
-	if (Theme == "default") delete localStorage.theme;
-	else localStorage.theme = Theme;
-
+	localStorage.theme = Theme;
 	LoadTheme();
 };
 
-LoadTheme();
+window.onload = function() {
+	LoadTheme();
+	document.getElementById("theme-selector").value = localStorage.theme;
+};
